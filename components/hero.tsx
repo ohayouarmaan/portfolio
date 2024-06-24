4b084fbe8510c928cd41bedeab08efcbd72b33fa
Armaan Gupta <ohayouarmaan@gmail.com>
2024-06-25 00:31:08 +0530

    feat: added threejs low poly computer

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,51 @@
 "use client";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useEffect, useRef, useState } from "react";
+import { Mesh, Vector3 } from "three";
+import Scene from "./Low_poly_computer";
+
+function Cube({position, color}: {position: number[], color?: string}) {
+    const cube_mesh = useRef<Mesh>(null);
+    const [mousePosition, setMousePosition] = useState<{x: number, y: number}>({x: 0, y: 0});
+    const [lightPosition, setLightPosition] = useState<{x: number, y: number}>({x: 0, y: 0});
+    window.addEventListener("mousemove", (event) => {
+        setMousePosition({
+            x: (event.clientX - (window.screen.width / 2)) / window.screen.width,
+            y: (event.clientY - (window.screen.height / 2)) / window.screen.height,
+        });
+        setLightPosition({
+            x: event.clientX / window.innerWidth,
+            y: event.clientY / window.innerHeight
+        });
+    })
+    useFrame((state, delta) => {
+        if(cube_mesh.current) {
+            cube_mesh.current.rotation.y = -mousePosition.x + 300
+            cube_mesh.current.rotation.x = -mousePosition.y
+        };
+    });
+    return (
+        <mesh ref={cube_mesh} position={position as unknown as Vector3}>
+            <ambientLight intensity={0.5} />
+    
+            <directionalLight
+                position={[5, 5, 5]}
+                intensity={1}
+                castShadow
+                shadow-mapSize-width={1024}
+                shadow-mapSize-height={1024}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+
+            <pointLight position={[-5, -5, -5]} intensity={0.5} />
+            <Scene rotation={[0,0,0]} />
+        </mesh>
+    )
+}
 
 export default function Hero() {
    return (<div className="flex w-full h-[90vh]">
@@ -8,6 +55,11 @@ export default function Hero() {
             <h1 className="text-neutral-300 text-[2rem]">my name is Armaan and I code.</h1>
             <h3 className="text-neutral-400 text-[1.2rem]">ps: I sometimes make music as well</h3>
         </div>
+    </div>
+    <div className="flex flex-col w-[50%] items-start">
+    <Canvas>
+        <Cube position={[0,0,0]} />
+    </Canvas>
     </div>
    </div>)
 }
